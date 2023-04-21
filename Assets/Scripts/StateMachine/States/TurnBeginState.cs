0471d4c8a1bf308d5626091299fe538ea9274e9c
Daniel David Frydman <danielfrydman@id.uff.br>
2023-04-21 00:25:14 -0300

    Novo estado TurnBeginState para início da rodada e arquivo meta

---
+++ Assets/Scripts/StateMachine/States/TurnBeginState.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class TurnBeginState : State
+{
+    public override async void Enter(){
+        if (machine.currentlyPlaying == machine.player1)
+            machine.currentlyPlaying = machine.player2;
+        else
+            machine.currentlyPlaying = machine.player1;
+
+        await Task.Delay(100);
+        machine.ChangeTo<TurnEndState>();
+    }
+}
