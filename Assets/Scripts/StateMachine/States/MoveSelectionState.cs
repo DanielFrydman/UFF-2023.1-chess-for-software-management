1e6d8f798724eee5dd718345c93a421f6c0ebe48
Daniel Frydman <danielfrydman@id.uff.br>
2023-04-21 11:39:45 -0300

    Criar um MoveSelectionState para as peças selecionadas

---
+++ Assets/Scripts/StateMachine/States/MoveSelectionState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSelectionState : State
+{
+    public override void Enter(){
+        
+    }
+}
