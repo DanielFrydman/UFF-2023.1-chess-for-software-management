3f029434a42eab4848167329d6fafab0be752737
Natan Bueno <72169218+Natan-Bueno@users.noreply.github.com>
2023-04-21 19:42:36 -0300

    Movimento Rainha e script das outras peças

---
+++ Assets/Scripts/Movement/RookMovement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RookMovement : Movement
+{
+    public override List<Tile> GetValidMoves(){
+        return null;
+    }
+}
