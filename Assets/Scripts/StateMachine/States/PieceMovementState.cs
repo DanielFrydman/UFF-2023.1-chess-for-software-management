628f7dd1554aa734f04cdd67bad16d33ee45a907
Daniel Frydman <danielfrydman@id.uff.br>
2023-04-21 13:47:10 -0300

    Criar PieceMovementState para movimento das peças

---
+++ Assets/Scripts/StateMachine/States/PieceMovementState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceMovementState : State
+{
+    public override void Enter(){
+        
+    }
+}
