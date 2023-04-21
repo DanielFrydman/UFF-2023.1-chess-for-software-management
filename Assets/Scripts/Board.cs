f10c3abaaaa23193ab88d773f494eb4c12e8e494
Daniel David Frydman <danielfrydman@id.uff.br>
2023-04-21 00:02:51 -0300

    Criar o load das peças ao começar o jogo

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,30 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class Board : MonoBehaviour
 {
     public static Board instace;
     public Dictionary<Vector2Int, Tile> tiles = new Dictionary<Vector2Int, Tile>();
+    public Transform goldHolder;
+    public Transform greenHolder;
     public List<Piece> goldPieces = new List<Piece>();
     public List<Piece> greenPieces = new List<Piece>();
     void Awake(){
         instace = this;
-        CreateBoard();
+    }
+    public async Task Load(){
+        GetTeams();
+
+        await Task.Run(() => CreateBoard());
+    }
+    void GetTeams(){
+        goldPieces.AddRange(goldHolder.GetComponentsInChildren<Piece>());
+        greenPieces.AddRange(greenHolder.GetComponentsInChildren<Piece>());
     }
     public void AddPiece(string team, Piece piece){
         Vector2 v2Pos = piece.transform.position;
         Vector2Int pos = new Vector2Int((int)v2Pos.x, (int)v2Pos.y);
         piece.tile = tiles[pos];
         piece.tile.content = piece;
-
-        if(team== "GoldPieces"){
-            goldPieces.Add(piece);
-        }else{
-            greenPieces.Add(piece);
-        }
     }
-    void CreateBoard(){
+    public void CreateBoard(){
         for (int i = 0; i < 8; i++){
             for (int j = 0; j < 8; j++){
                 CreateFile(i, j);
