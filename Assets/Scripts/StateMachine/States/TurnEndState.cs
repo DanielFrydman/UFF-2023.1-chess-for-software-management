8928ff224b1f132cdbd5234171b96f5e6effead7
Daniel David Frydman <danielfrydman@id.uff.br>
2023-04-21 00:24:52 -0300

    Novo estado TurnEndState para final da rodada e arquivo meta

---
+++ Assets/Scripts/StateMachine/States/TurnEndState.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class TurnEndState : State
+{
+    public override async void Enter(){
+        bool gameFinished = CheckTeams();
+        await Task.Delay(100);
+        if (gameFinished)
+            machine.ChangeTo<GameEndState>();
+        else
+            machine.ChangeTo<TurnBeginState>();
+    }
+    bool CheckTeams(){
+        Piece goldPiece = Board.instace.goldPieces.Find((x) => x.gameObject.activeSelf == true);
+        if (goldPiece == null){
+            return true;
+        }
+
+        Piece greenPiece = Board.instace.greenPieces.Find((x) => x.gameObject.activeSelf == true);
+        if (greenPiece == null){
+            return true;
+        }
+
+        return false;
+    }
+}
