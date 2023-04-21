38ce0bcb65f07e12f1e9ea2beae0f30cc2c324a3
Daniel David Frydman <danielfrydman@id.uff.br>
2023-04-21 00:03:02 -0300

    Carregar as peças no estado do jogo

--- Assets/Scripts/StateMachine/LoadState.cs
+++ Assets/Scripts/StateMachine/LoadState.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class LoadState : State
 {
-    public override void Enter(){
-        
+    public override async void Enter(){
+        await Board.instace.Load();
+        await LoadAllPiecesAsync();
+        //machine.ChangeTo<TurnBeginState>();
+    }
+    async Task LoadAllPiecesAsync(){
+        LoadTeamPieces(Board.instace.greenPieces);
+        LoadTeamPieces(Board.instace.goldPieces);
+        await Task.Delay(100);
+    }
+    void LoadTeamPieces(List<Piece> pieces){
+        foreach(Piece piece in pieces){
+            Board.instace.AddPiece(piece.transform.parent.name, piece);
+        }
     }
 }
