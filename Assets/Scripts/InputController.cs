610d223571a0acc9333a19c44f19214e86df4cab
Matheus <mvsilva@id.uff.br>
2023-04-26 11:09:18 -0300

    criação de classe InputController paracomando de estado das peças

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void TileClickedEvent(object sender, object args);
+public class InputController : MonoBehaviour
+{
+    
+    public static InputController instance;
+    public TileClickedEvent tileClicked = delegate{};
+    public TileClickedEvent returnClicked = delegate{};
+    void Awake(){
+        instance = this;
+    }
+    void Update(){
+        if(Input.GetButtonDown("Cancel")){
+            returnClicked(null, null);
+        }
+    }
+}
