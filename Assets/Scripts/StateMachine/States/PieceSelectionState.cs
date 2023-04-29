49049bdf12bcab051ae43fe698d5fde6644d802e
Matheus <mvsilva@id.uff.br>
2023-04-29 16:59:20 -0300

    Corrigindo erro de colisão de peças

--- Assets/Scripts/StateMachine/States/PieceSelectionState.cs
+++ Assets/Scripts/StateMachine/States/PieceSelectionState.cs
@@ -6,9 +6,11 @@ public class PieceSelectionState : State
 {
     public override void Enter(){
         InputController.instance.tileClicked += PiceClicked;
+        SetColliders(true);
     }
     public override void Exit(){
         InputController.instance.tileClicked -= PiceClicked;
+        SetColliders(false);
     }
     void PiceClicked(object sender, object args){
         Piece piece = sender as Piece;
@@ -17,5 +19,18 @@ public class PieceSelectionState : State
             Board.instance.selectedPiece = piece;
             machine.ChangeTo<MoveSelectionState>();
         }
+    }
+
+    /*void SetColliders(bool State){
+        foreach(BoxCollider2D b in machine.currentlyPlaying.GetComponentInChildren<BoxCollider2D>()){
+            b.enabled = state;
+        }
+    }   <-------------- forma que deveria estar e não funcionou*/
+
+    void SetColliders(bool state){                                                                     //<--------------------Forma alterado funcionou
+    BoxCollider2D[] colliders = machine.currentlyPlaying.GetComponentsInChildren<BoxCollider2D>();
+    foreach(BoxCollider2D b in colliders){
+        b.enabled = state;
+        } 
     }
 }
