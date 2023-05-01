1d1c0640e53e40fd089b3bd60b2d9354c57775cd
Daniel Frydman <danielfrydman@id.uff.br>
2023-05-01 09:20:28 -0300

    Atribuindo valor ao bispo

--- Assets/Scripts/Movement/BishopMovement.cs
+++ Assets/Scripts/Movement/BishopMovement.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class BishopMovement : Movement
 {
+    public BishopMovement(){
+        value = 3;
+    }
     public override List<Tile> GetValidMoves(){
         List<Tile> moves = new List<Tile>();
-        
 
         moves.AddRange(UntilBlockedPath(new Vector2Int(1, 1), true, 99));
         moves.AddRange(UntilBlockedPath(new Vector2Int(1, -1), true, 99));
