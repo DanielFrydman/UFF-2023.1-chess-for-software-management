b410d6b3b4566b6e7ca301e730477111e51aeb50
Daniel Frydman <danielfrydman@id.uff.br>
2023-05-01 09:19:22 -0300

    Criando ply para sistema de evaluation

---
+++ Assets/Scripts/AI/Ply.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ply
+{
+    public List<PieceEvaluation> golds;
+    public List<PieceEvaluation> greens;
+    public float score;
+}
