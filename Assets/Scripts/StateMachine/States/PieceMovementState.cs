4eaf9c978b4a8ad0e2d8352a4465b7fbd6f9edad
Daniel Frydman <danielfrydman@id.uff.br>
2023-05-01 09:54:44 -0300

    Refatorando PieceMovementState

--- Assets/Scripts/StateMachine/States/PieceMovementState.cs
+++ Assets/Scripts/StateMachine/States/PieceMovementState.cs
@@ -5,54 +5,71 @@ using System.Threading.Tasks;
 
 public class PieceMovementState : State
 {
+    public static List<AffectedPiece> changes;
     public override async void Enter(){
-
-        MoveType movetype = Board.instance.selectedHighlight.tile.moveType;
-        ClearEnPassants();
-
         TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
-        switch(movetype){
-            case MoveType.Normal:
-                NormalMove(tcs);
-                break;
-            case MoveType.Castling:
-                Castling(tcs);
-                break;
-            case MoveType.PawnDoubleMove:
-                PawnDoubleMove(tcs);
-                break;
-            case MoveType.EnPassant:
-                EnPassant(tcs);
-                break;
-            case MoveType.Promotion:
-                Promotion(tcs);
-                break;
-        }
-
+        MovePiece(tcs, false);
 
         await tcs.Task;
         machine.ChangeTo<TurnEndState>();
     }
-    void NormalMove(TaskCompletionSource<bool> tcs){
+    public static void MovePiece(TaskCompletionSource<bool> tcs, bool skipMovements){
+        changes = new List<AffectedPiece>();
+        MoveType movetype = Board.instance.selectedHighlight.tile.moveType;
+        ClearEnPassants();
+
+        switch(movetype){
+            case MoveType.Normal:
+                NormalMove(tcs, skipMovements);
+                break;
+            case MoveType.Castling:
+                Castling(tcs, skipMovements);
+                break;
+            case MoveType.PawnDoubleMove:
+                PawnDoubleMove(tcs, skipMovements);
+                break;
+            case MoveType.EnPassant:
+                EnPassant(tcs, skipMovements);
+                break;
+            case MoveType.Promotion:
+                Promotion(tcs, skipMovements);
+                break;
+        }
+    }
+    static void NormalMove(TaskCompletionSource<bool> tcs, bool skipMovements){
         Piece piece = Board.instance.selectedPiece;
+        AffectedPiece pieceMoving = new AffectedPiece();
+        pieceMoving.piece = piece;
+        pieceMoving.from = piece.tile;
+        changes.Add(pieceMoving);
+
         piece.tile.content = null;
         piece.tile = Board.instance.selectedHighlight.tile;
 
         if(piece.tile.content != null){
             Piece deadPiece = piece.tile.content;
+            AffectedPiece pieceKilled = new AffectedPiece();
+            pieceKilled.piece = deadPiece;
+            pieceKilled.from = piece.tile;
+            changes.Add(pieceKilled);
             deadPiece.gameObject.SetActive(false);
         }
 
         piece.tile.content = piece;
         piece.wasMoved = true;
 
-        float timing = Vector3.Distance(piece.transform.position, Board.instance.selectedHighlight.transform.position)*0.5f;
-        LeanTween.move(piece.gameObject, Board.instance.selectedHighlight.transform.position, timing).
-            setOnComplete(()=> {
+        if(skipMovements){
+            piece.transform.position = Board.instance.selectedHighlight.transform.position;
+            tcs.SetResult(true);
+        }else{
+            float timing = Vector3.Distance(piece.transform.position, Board.instance.selectedHighlight.transform.position)*0.5f;
+            LeanTween.move(piece.gameObject, Board.instance.selectedHighlight.transform.position, timing).
+                setOnComplete(()=> {
                 tcs.SetResult(true);
             });
+        }
     }
-    void Castling(TaskCompletionSource<bool> tcs){
+    static void Castling(TaskCompletionSource<bool> tcs, bool skipMovements){
         Piece king = Board.instance.selectedPiece;
         king.tile.content = null;
         Piece rook = Board.instance.selectedHighlight.tile.content;
@@ -79,26 +96,26 @@ public class PieceMovementState : State
         LeanTween.move(rook.gameObject, new Vector3(rook.tile.pos.x, rook.tile.pos.y, 0), 1.4f);
 
     }
-   void ClearEnPassants(){
+   static void ClearEnPassants(){
     ClearEnPassants(5);
     ClearEnPassants(2);
    }
-   void ClearEnPassants(int height){
+   static void ClearEnPassants(int height){
         Vector2Int positions = new Vector2Int(0, height);
         for(int i=0; i<7; i++){
             positions.x = positions.x+1;
             Board.instance.tiles[positions].moveType = MoveType.Normal;
         }
     }
-    void PawnDoubleMove(TaskCompletionSource<bool> tsc){
+    static void PawnDoubleMove(TaskCompletionSource<bool> tsc, bool skipMovements){
         Piece pawn = Board.instance.selectedPiece;
         Vector2Int direction = pawn.tile.pos.y > Board.instance.selectedHighlight.tile.pos.y ?
             new Vector2Int(0, -1):
             new Vector2Int(0, 1);
         Board.instance.tiles[pawn.tile.pos+direction].moveType = MoveType.EnPassant;
-        NormalMove(tsc);
+        NormalMove(tsc, skipMovements);
     }
-    void EnPassant(TaskCompletionSource<bool> tsc){
+    static void EnPassant(TaskCompletionSource<bool> tsc, bool skipMovements){
         Piece pawn = Board.instance.selectedPiece;
         Vector2Int direction = pawn.tile.pos.y > Board.instance.selectedHighlight.tile.pos.y ?
             new Vector2Int(0, 1):
@@ -106,11 +123,11 @@ public class PieceMovementState : State
         Tile enemy = Board.instance.tiles[Board.instance.selectedHighlight.tile.pos+direction];
         enemy.content.gameObject.SetActive(false);
         enemy.content = null;
-        NormalMove(tsc);
+        NormalMove(tsc, skipMovements);
     }
-    async void Promotion(TaskCompletionSource<bool> tsc){
+    static async void Promotion(TaskCompletionSource<bool> tsc, bool skipMovements){
         TaskCompletionSource<bool> movementTCS = new TaskCompletionSource<bool>();
-        NormalMove(movementTCS);
+        NormalMove(movementTCS, skipMovements);
         await movementTCS.Task;
         
         StateMachineController.instance.taskHold = new TaskCompletionSource<object>();
