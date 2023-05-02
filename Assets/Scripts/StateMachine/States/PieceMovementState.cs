690fb743bd16c4f910c33770d52d2531b4ec850c
Daniel David Frydman <danielfrydman@id.uff.br>
2023-05-02 06:48:26 -0300

    Mover lógica para dentro de else

--- Assets/Scripts/StateMachine/States/PieceMovementState.cs
+++ Assets/Scripts/StateMachine/States/PieceMovementState.cs
@@ -56,12 +56,12 @@ public class PieceMovementState : State
         }
 
         piece.tile.content = piece;
-        piece.wasMoved = true;
 
         if(skipMovements){
             piece.transform.position = Board.instance.selectedHighlight.transform.position;
             tcs.SetResult(true);
         }else{
+            piece.wasMoved = true;
             float timing = Vector3.Distance(piece.transform.position, Board.instance.selectedHighlight.transform.position)*0.5f;
             LeanTween.move(piece.gameObject, Board.instance.selectedHighlight.transform.position, timing).
                 setOnComplete(()=> {
