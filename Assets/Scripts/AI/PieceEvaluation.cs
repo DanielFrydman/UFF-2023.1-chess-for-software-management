fe657a478daf5fdb40a7eb1948ad9bedb24a452c
Daniel David Frydman <danielfrydman@id.uff.br>
2023-06-06 16:31:22 -0300

    Removendo lista de moves de piece evaluation

--- Assets/Scripts/AI/PieceEvaluation.cs
+++ Assets/Scripts/AI/PieceEvaluation.cs
@@ -1,9 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PieceEvaluation
 {
     public Piece piece;
-    public List<AvailableMove> availableMoves;
 }
